15f03bc88fb928426e9de06d1392a5d4972fdf77
Biel <bielbcm@gmail.com>
2019-03-21 11:29:16 +0100

    Update name

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import BGImage from './bg-image'
-import LayoutNoFooter from './layout-no-footer'
 import Layout from './layout'
-import Split from './split'
 import Theme from './theme'
 import webpackRules from './webpackRules'
 
-export { BGImage, LayoutNoFooter, Layout, Split, webpackRules }
+export { Layout, webpackRules }
 
 export default Theme
